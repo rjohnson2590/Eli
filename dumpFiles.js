77d59770bdda043253eb2f8e2f118f7dc10ec0c1
Richard Johnson <rjohnson2590@gmail.com>
2015-05-10 19:46:47 -0700

    figure out how to do async shit with the two files, compare and such

--- dumpFiles.js
+++ dumpFiles.js
@@ -10,38 +10,40 @@ commander
 	.option('--read','read the fucking file')
 	.parse(process.argv);
 
+function logArrayElements(element, index, array) {
+	if(element.index === element.index-1){
+		console.log("hello")
+	}
+  // console.log('a[' + index + '] = ' + element);
+}
+
+
 var filenames = commander.args;
-var noo=[];
 filenames.forEach(function(filename) {
 	fs.readFile(filename, {encoding: 'utf8'}, function(error, contents){
-		var arg=[];
-		var len= contents.split(" ")
-		// console.log(len.length);
-		arg.push(len);
-		console.log(arg)
-	});
-});
+		var newContent1 = contents.replace(/[\.,]/g, '');
+		var newArray1= newContent1.split(" ")
+		console.log(newArray1);
+		newArray1.forEach(logArrayElements);
+			
+		});
+		});
+			
+
+
+
 
 
 // function dumpfiles(filename) {
 // 	fs.readFile(filename, {encoding: 'utf8'}, function(
 // 		error, contents){
-// 		var len= contents.split(" ")
-// 		console.log(len.length);
-
-
+// 		var newArray1= contents.split(" ")
+// 		console.log(newArray1);
 // 		if(filenames.length > 0){
 // 		dumpfiles(filenames.pop());
 // 	}	
 // 	});
-// 	fs.readFile(filename, {encoding: 'utf8'}, function(
-// 		error, contents){
-// 		console.log(contents);
-
-// 		if(filenames.length > 0){
-// 		dumpfiles(filenames.pop());
-// 	}
-// });
+	
 // }
 
 // if(commander.read){
